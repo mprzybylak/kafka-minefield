1b281c48315bd256ec428ecc8cd386a744238813
Michał Przybylak <michal.przybylak@gmail.com>
2017-10-12 23:17:26 +0200

    producing message

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 import java.util.Properties
 
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object Main {
 
@@ -9,7 +9,11 @@ object Main {
     val producerProperties = new Properties()
     producerProperties.put("bootstrap.servers", "127.0.0.1:9092")
     producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    producerProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
     val producer = new KafkaProducer[String, String](producerProperties)
+
+    val record = new ProducerRecord[String, String]("test", "key", "value")
+    producer.send(record);
   }
 }
