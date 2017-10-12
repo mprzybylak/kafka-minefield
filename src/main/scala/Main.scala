a15ed446a542da874ba0c41735e8a85fc7546fca
Michał Przybylak <michal.przybylak@gmail.com>
2017-10-12 23:11:12 +0200

    creation of simple konsumer

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,15 @@
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.KafkaProducer
+
 object Main {
+
   def main(args: Array[String]): Unit = {
 
+    val producerProperties = new Properties()
+    producerProperties.put("bootstrap.servers", "127.0.0.1:9092")
+    producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String, String](producerProperties)
   }
 }
